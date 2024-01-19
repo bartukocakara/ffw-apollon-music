5e632a2bccaafe8fcd22431ef5aec9dbfa74041b
bartukocakara <kocakarabartu@gmail.com>
2024-01-19 22:26:09 +0300

    profile fix

--- app/Http/Controllers/Customer/ProfileController.php
+++ app/Http/Controllers/Customer/ProfileController.php
@@ -17,7 +17,7 @@ class ProfileController extends Controller
      */
     public function edit(Request $request): View
     {
-        return view('customer.profile.edit', [
+        return view('customer.profile.contents.edit', [
             'user' => $request->user(),
         ]);
     }
--- resources/views/customer/profile/components/sidebar.blade.php
+++ resources/views/customer/profile/components/sidebar.blade.php
@@ -7,18 +7,22 @@
              <i class="bx bx-refresh"></i>
              </button>
           </div>
-          <h2 class="h5 mb-1">John Doe</h2>
-          <p class="mb-3 pb-3">jonny@email.com</p>
+          <h2 class="h5 mb-1">{{ Auth::user()->name }}</h2>
+          <p class="mb-3 pb-3">{{ Auth::user()->email }}</p>
           <button type="button" class="btn btn-secondary w-100 d-md-none mt-n2 mb-3" data-bs-toggle="collapse" data-bs-target="#account-menu">
           <i class="bx bxs-user-detail fs-xl me-2"></i>
           Account menu
           <i class="bx bx-chevron-down fs-lg ms-1"></i>
           </button>
           <div id="account-menu" class="list-group list-group-flush collapse d-md-block">
-             <a href="{{ route('customer.conversions.index') }}" class="list-group-item list-group-item-action d-flex align-items-center">
-             <i class="bx bx-collection fs-xl opacity-60 me-2"></i>
-                Conversions
-             </a>
+                <a href="{{ route('customer.profile.edit') }}" class="list-group-item list-group-item-action d-flex align-items-center active">
+                    <i class="bx bx-cog fs-xl opacity-60 me-2"></i>
+                    Accountt Details
+                </a>
+                <a href="{{ route('customer.conversions.index') }}" class="list-group-item list-group-item-action d-flex align-items-center">
+                    <i class="bx bx-collection fs-xl opacity-60 me-2"></i>
+                    Conversions
+                </a>
              <a href="account-payment.html" class="list-group-item list-group-item-action d-flex align-items-center">
              <i class="bx bx-credit-card-front fs-xl opacity-60 me-2"></i>
              Payment Details
