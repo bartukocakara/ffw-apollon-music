f5eedf2f20bba9c31c8c2d68175921b924910937
bartukocakara <kocakarabartu@gmail.com>
2024-02-04 01:57:02 +0300

    share app

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::get('/test-api', [TestApiController::class,'index'])->name('test-api');
-Route::get('/denemeeee', function() {
+Route::get('/deneme', function() {
     dd(123);
 });
 
